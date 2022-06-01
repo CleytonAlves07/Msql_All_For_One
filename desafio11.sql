4033c9ab58adac37fbb6da34c0eed944038c09da
Cleyton Alves <cleyton.alves.a@gmail.com>
2022-06-01 18:30:09 -0300

    Requisito 11

--- desafio11.sql
+++ desafio11.sql
@@ -1,9 +1,2 @@
--- SELECT notes FROM cnorthwind.purchase_orders
--- WHERE notes LIKE '%#' >= 30
--- AND notes LIKE '%#' <= 39;
-
--- SELECT notes FROM cnorthwind.purchase_orders
--- WHERE notes BETWEEN '%30' AND '%39';
-
--- SELECT notes FROM cnorthwind.purchase_orders 
--- WHERE notes IN ('30','31');
+SELECT notes FROM northwind.purchase_orders
+WHERE notes LIKE '%3%';
